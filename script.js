f4321257d4a02f59c687b71a0894d042300bd740
Pallavi <pallavishettigar03@gmail.com>
2023-11-21 10:49:15 +0530

    add changes

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let preveiwContainer = document.querySelector('.products-preview');
 let previewBox = preveiwContainer.querySelectorAll('.preview');
 
-document.querySelectorAll('.products-container .product').forEach(product =>{
+document.querySelectorAll('.products-containerr .product').forEach(product =>{
   product.onclick = () =>{
     preveiwContainer.style.display = 'flex';
     let name = product.getAttribute('data-name');
@@ -20,3 +20,4 @@ previewBox.forEach(close =>{
     preveiwContainer.style.display = 'none';
   };
 });
+
